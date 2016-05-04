655fdcd1f5fbfec3043c04431b9d4415b0503b23
Epicurean306 <benjaminforbesgriffith@gmail.com>
2016-05-04 14:35:59 -0400

    setup HTML form hooks, JavaScript objects/arrays for Q&A-banks, tweaked Bootstrap styles, added a Google-font titleHeader

--- assets/scripts/trivia.js
+++ assets/scripts/trivia.js
@@ -1,3 +1,20 @@
 (function (window) {
   'use strict';
+
+  /* Q&A object-array */
+  var questionBank = [
+    { QobjectIndexer: 0, question: "Who is the writer of Lord of the Rings and founding father of the Middle-Earth Saga?", answer: "J.R.R. Tolkien" },
+    { QobjectIndexer: 1, question: "Frodo and Bilbo are...", answer: "hobbits" },
+    { QobjectIndexer: 2, question: "What is Strider's real name?", answer: "Aragorn" },
+    { QobjectIndexer: 3, question: "What is the name of Boromir's brother?", answer: "Faramir" }
+  ]
+
+  /* incorrect-guesses object-array */
+  var wrongChoices = [
+    ["Christopher Tolkien", "George R.R. Martin", "C.S. Lewis"],
+    ["dwarves", "elves", "gnomes"],
+    ["Legolas", "Boromir", "Thorin"],
+    ["Denethor", "Thrain", "Eorl"]
+  ]
+
 })(window);
