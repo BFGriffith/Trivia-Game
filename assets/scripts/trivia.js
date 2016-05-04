30be07ca4e828c55151837c94a0d76983e1222cc
Epicurean306 <benjaminforbesgriffith@gmail.com>
2016-05-04 16:51:01 -0400

    got MVP working, displays results at timeout

--- assets/scripts/trivia.js
+++ assets/scripts/trivia.js
@@ -1,6 +1,13 @@
 (function (window) {
   'use strict';
 var time, counter, currentTime;
+
+/* stores tally-counts into array */
+var tallyCount = [rightAnswers, wrongAnswers, unanswered];
+var rightAnswers = 0;
+var wrongAnswers = 0;
+var unanswered = 0;
+
   /* Q&A object-array */
   var questionBank = [
     { QobjectIndexer: 0, question: "Who is the writer of Lord of the Rings and founding father of the Middle-Earth Saga?", answer: "J.R.R. Tolkien" },
@@ -63,16 +70,39 @@ $(document).ready(function() {
   setTimeout(function() {
     alert('Welcome to the Shire, traveler. Concerning hobbits, to gain entry you must test your knowledge of Middle Earth. Once you clear this message, your Riddles in the Dark will begin! All we have to decide is what to do with the time that is given us.');
   }, 1000);
-  time = 600;
-  $("#timerDisplay").html("05:00");
+  time = 60;
+  $("#timerDisplay").html("00:00");
 
   function clockView() {
     time--;
     currentTime = timeConverter(time);
     $("#timerDisplay").html(currentTime);
     // console.log(time);
-  };
+
+    if (time == 0) {
+      for (var z = 0; z < questionBank.length; z++) {
+        if ($('#question' + z + ':checked').val() == questionBank[z].answer) {
+          rightAnswers++;
+        } else if ($('#question' + z + ':checked').val() == undefined) {
+          unanswered++;
+        } else {
+          wrongAnswers++;
+        }
+      }
+      $('#questionBox').empty();
+      $('#questionBox').append("<h2>Behold the fate of Middle Earth before you vanish into the West!</h2>");
+      $('#questionBox').append("<p>You got " + rightAnswers + " answers right!</p>");
+      $('#questionBox').append("<p>You got " + wrongAnswers + " answers wrong.</p>");
+      $('#questionBox').append("<p>There are " + unanswered + " unanswered questions remaining.</p>");
+      time = undefined;
+      $("#timerDisplay").html(currentTime);
+    }
+  }
   counter = setInterval(clockView, 1000);
 })
+
+console.log(rightAnswers);
+console.log(wrongAnswers);
+console.log(unanswered);
 
 })(window);
